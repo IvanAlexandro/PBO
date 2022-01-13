b6301a3fb8efa2d4b1a751b902683ae23625c02d
IvanAlexandro <ivanalexandro@uvers.ac.id>
2022-01-13 22:05:14 +0700

    Pertemuan1

---
+++ Pertemuan1/HalloDunia/src/hallodunia/HalloDunia.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hallodunia;
+
+import static java.time.Clock.system;
+
+/**
+ *
+ * @author User
+ */
+public class HalloDunia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ system.out.println("halo Dunia Satu Keluarga");
+system.out.println("coba lagi");
+    }
+}
